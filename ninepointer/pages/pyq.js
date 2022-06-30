b8397fcdc762710456fa6de270e5a1d34bfef68f
Anshuman <anshuman.sharma000@gmail.com>
2022-06-30 22:21:47 +0530

    api request to server

---
+++ ninepointer/pages/pyq.js
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from '../styles/pyq.module.scss';
+
+const pyq = () => {
+  const [searchData, setSearchData] = useState({
+    university: '',
+    semester: '',
+    branch: '',
+    subject: '',
+  });
+  const handleClick = async () => {
+    console.log(searchData.subject);
+    const query = `?university=${searchData.university}&branch=${searchData.branch}&semester=${searchData.semester}&subject=${searchData.subject}`;
+    console.log(query);
+
+    const url = `https://ninepointer-staging.herokuapp.com/api/v1/engineering/pyq`;
+    const devUrl = 'http://localhost:8000/api/v1/engineering/pyq';
+    const queryUrl = url + query;
+    console.log(queryUrl);
+
+    const response = await axios.get(queryUrl);
+    console.log(response.data.data);
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setSearchData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className={styles.container}>
+      <section>
+        <label htmlFor='university'>University</label>
+        <select name='university' id='university' onChange={handleChange}>
+          <option value='' disabled selected>
+            Select your option
+          </option>
+          <option value='NIT Rourkela'>NIT Rourkela</option>
+          <option value='BPUT'>Biju Pattnaik University of Technology</option>
+          <option value='IIT Bombay'>IIT Bombay</option>
+          <option value='RTU'>Rajasthan Technical University(RTU)</option>
+          <option value='VJTU'>VJTU, Mumbai</option>
+          <option value='AKTU'>
+            Abdul Kalam Technical University, Lucknow
+          </option>
+        </select>
+        <label htmlFor='semester'>Semester</label>
+        <select name='semester' id='semester' onChange={handleChange}>
+          <option value='' disabled selected>
+            Select your option
+          </option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+          <option value='6'>6</option>
+          <option value='7'>7</option>
+          <option value='8'>8</option>
+        </select>
+        <label htmlFor='branch'>Branch</label>
+        <select name='branch' id='branch' onChange={handleChange}>
+          <option value='' disabled selected>
+            Select your option
+          </option>
+          <option value='Common'>Common</option>
+          <option value='Computer Science'>Computer Science</option>
+          <option value='Electrical'>Electrical</option>
+          <option value='Electronics'>Electronics</option>
+          <option value='Chemical'>Chemical</option>
+          <option value='Civil'>Civil</option>
+          <option value='Mechanical'>Mechanical</option>
+          <option value='Textile'>Textile</option>
+          <option value='Ceramic'>Ceramic</option>
+          <option value='Biomedical'>Biomedical</option>
+          <option value='Metallurgy'>Metallurgy</option>
+          <option value='Metallurgy'>Others</option>
+        </select>
+        <label htmlFor='subject'>Subject</label>
+        <input
+          type='text'
+          name='subject'
+          value={searchData.subject}
+          onChange={handleChange}
+        />
+        <button onClick={handleClick}>Search</button>
+      </section>
+    </div>
+  );
+};
+
+export default pyq;
--- ninepointer/pages/upload-pyq.js
+++ ninepointer/pages/upload-pyq.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import styles from '../styles/upload-pyq.module.scss';
 
 const uploadpyq = () => {
   const [formData, setFormData] = useState({
@@ -10,6 +11,8 @@ const uploadpyq = () => {
     subject: '',
     university: '',
     author: '',
+    type: '',
+    url: '',
     // file: '',
   });
 
@@ -30,14 +33,17 @@ const uploadpyq = () => {
     fd.append('branch', formData.branch);
     fd.append('semester', formData.semester);
     fd.append('year', formData.year);
+    fd.append('type', formData.type);
     fd.append('subject', formData.subject);
     fd.append('university', formData.university);
     fd.append('author', formData.author);
+    if (formData.url) fd.append('url', formData.url);
 
     console.log(fd);
 
     const response = await axios.post(
-      'http://localhost:8000/api/v1/engineering/pyq',
+      // 'http://localhost:8000/api/v1/engineering/pyq',
+      'https://ninepointer-staging.herokuapp.com/api/v1/engineering/pyq',
       fd,
       {
         headers: {
@@ -52,7 +58,7 @@ const uploadpyq = () => {
   };
 
   const handleChange = (e) => {
-    console.log(e);
+    // console.log(e);
     const value =
       e.target.type === 'checkbox' ? e.target.checked : e.target.value;
     const name = e.target.name;
@@ -65,30 +71,43 @@ const uploadpyq = () => {
   };
 
   return (
-    <div>
+    <div className={styles.container}>
+      <h1>Upload PYQs</h1>
       <form onSubmit={handleSubmit}>
         <section>
           <label htmlFor='branch'>Branch</label>
-          <input
-            type='text'
-            name='branch'
-            id='branch'
-            placeholder='Branch'
-            required={true}
-            value={formData.branch}
-            onChange={handleChange}
-          />
+          <select name='branch' id='branch' onChange={handleChange}>
+            <option value='' disabled selected>
+              Select your option
+            </option>
+            <option value='Common'>Common</option>
+            <option value='Computer Science'>Computer Science</option>
+            <option value='Electrical'>Electrical</option>
+            <option value='Electronics'>Electronics</option>
+            <option value='Chemical'>Chemical</option>
+            <option value='Civil'>Civil</option>
+            <option value='Mechanical'>Mechanical</option>
+            <option value='Textile'>Textile</option>
+            <option value='Ceramic'>Ceramic</option>
+            <option value='Biomedical'>Biomedical</option>
+            <option value='Metallurgy'>Metallurgy</option>
+            <option value='Metallurgy'>Others</option>
+          </select>
 
           <label htmlFor='semester'>Semester</label>
-          <input
-            type='text'
-            name='semester'
-            id='semester'
-            placeholder='Semester'
-            required={true}
-            value={formData.semester}
-            onChange={handleChange}
-          />
+          <select name='semester' id='semester' onChange={handleChange}>
+            <option value='' disabled selected>
+              Select your option
+            </option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+          </select>
 
           <label htmlFor='year'>Year</label>
           <input
@@ -110,13 +129,12 @@ const uploadpyq = () => {
             value={formData.subject}
             onChange={handleChange}
           />
-          <label htmlFor='type'>type</label>
-          <select>
-            <option value='Ford'>Ford</option>
-            <option value='Volvo' selected>
-              Volvo
-            </option>
-            <option value='Fiat'>Fiat</option>
+          <label htmlFor='type'>Question Type</label>
+          <select name='type' onChange={handleChange}>
+            <option value='Regular'>Regular</option>
+            <option value='Back'>Back</option>
+            <option value='Both'>Both</option>
+            <option value='Others'>Others</option>
           </select>
           <label htmlFor='university'>University</label>
           <input
@@ -154,6 +172,14 @@ const uploadpyq = () => {
             value={formData.branch}
             onChange={handleChange}
           /> */}
+          <input
+            type='url'
+            name='url'
+            id='url'
+            placeholder='Add link to file'
+            value={formData.url}
+            onChange={handleChange}
+          />
           <input type='submit' />
         </section>
       </form>
