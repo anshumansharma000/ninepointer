2816a4bd663e676fdfbf46f3a1452dbeb74fa00d
infinitelyinfinite-3108 <agarwalchandrika03@gmail.com>
2022-08-18 21:26:40 +0530

    ..........

---
+++ ninepointer/components/VideoCard.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+import React from 'react';
+import styles from '../styles/videoCard.module.scss';
+
+const VideoCard = (props) => {
+  return (
+    <>
+      <div className={styles.videoCards}>
+        <div className={styles.videoCard}>
+          <div className={styles.thumbnail}>
+          <a href={props.link} target='_blank'>
+          <div className={styles.thumbnailWrapper}><img  src={props.img_src} className={styles.cardThumbnail} /> 
+          </div></a>
+          </div>
+          
+          <div className={styles.reference}>
+          <a href={props.link} target='_blank'>
+            <h3 className={styles.cardTitle}>{props.cardTitle}</h3> </a>
+            <br />
+            <a href={props.link} target='_blank'>
+            <div className={styles.cardDescription}>
+
+              {props.cardDescription}</div> </a>
+              <br />
+              <span className={styles.cardAuthor}>{props.cardAuthor} </span>
+              <br />
+              <a href={props.link} target='_blank'>
+                <br />
+                <br/>
+                <br/>
+                <div className={styles.button}><span> Watch now</span> </div>
+        
+              </a>
+              <br/>
+            </div>{' '}
+          </div>
+        </div>
+      
+    </>
+  );
+};
+export default VideoCard;
---
+++ ninepointer/pages/videoCard.js
@@ -0,0 +1,46 @@
+import VideoCard from '../components/VideoCard';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
+const vdo = () => {
+  const [loading, setLoading] = useState();
+  const [data, setData] = useState();
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      
+      setLoading();
+      const response = await axios.get(
+        `https://ninepointer-staging.herokuapp.com/api/v1/engineering/video`
+      );
+      console.log(response.data.data);
+      setData(response.data.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <VideoCard
+        img_src='https://images.pexels.com/photos/1987151/pexels-photo-1987151.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+        cardTitle=' Learn Web Development'
+        cardDescription='this video enables you to learn web dev from grass root level and gradually touch the skies.'
+        cardAuthor='apple'
+        link='https://drive.google.com/file/d/16su5NEKPbQFqO0OT0EyTkigKDiXpLKDg/view?usp=sharing'
+      />
+      <VideoCard
+        img_src='https://images.pexels.com/photos/1987151/pexels-photo-1987151.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+        cardTitle=' Learn Web Development'
+        cardDescription='this video enables you to learn web dev from grass root level and gradually touch the skies.'
+        cardAuthor='apple'
+        link='https://drive.google.com/file/d/16su5NEKPbQFqO0OT0EyTkigKDiXpLKDg/view?usp=sharing'
+      />
+    </>
+  );
+};
+
+// should be added 
+// should be done
+
+export default vdo;
