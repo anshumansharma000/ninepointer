d5a2776a4b400439a13afd0d97406c30e027f850
Anshuman <anshuman.sharma000@gmail.com>
2022-09-01 14:57:31 +0530

    Implemented filters for video fetch

--- ninepointer/pages/video.js
+++ ninepointer/pages/video.js
@@ -1,43 +1,158 @@
 import VideoCard from '../components/VideoCard';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styles from '../styles/video.module.scss';
 
 const video = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState();
+  const [branch, setBranch] = useState([]);
+  const [type, setType] = useState([]);
+  const [query, setQuery] = useState('');
   useEffect(() => {
     //fetch data here
-    const fetchData = async () => {
-      //data fetch
-      //set loading to true
-      setLoading(true);
-      const res = await axios.get(
-        `https://ninepointer-staging.herokuapp.com/api/v1/engineering/video`
-      );
-      setData(res.data.data);
-      setLoading(false);
-    };
     fetchData();
-  }, []);
+  }, [query]);
+
+  // useEffect(()=>{
+  //   fetchData();
+  // }, [query]);
+
+  const fetchData = async () => {
+    //data fetch
+    //set loading to true
+    setLoading(true);
+    console.log(query);
+    const res = await axios.get(
+      `https://ninepointer-staging.herokuapp.com/api/v1/engineering/video/${query}`
+    );
+    setData(res.data.data);
+    console.log(res.data.data);
+    setLoading(false);
+  };
+  const branchChange = (e) => {
+    if (e.target.checked === true) {
+      setBranch((prevState) => [...prevState, e.target.name]);
+      setQuery((prevState) =>
+        query == ''
+          ? prevState + `?&${e.target.className}=${e.target.name}`
+          : prevState + `&${e.target.className}=${e.target.name}`
+      );
+      // fetchData();
+    } else {
+      setBranch((prevState) => prevState.filter((i) => i !== e.target.name));
+      setQuery((prevState) =>
+        prevState.replace(`&${e.target.className}=${e.target.name}`, '') === '?'
+          ? ''
+          : prevState.replace(`&${e.target.className}=${e.target.name}`, '')
+      );
+      // fetchData();
+    }
+  };
+
+  const fetchVideos = async () => {
+    setLoading(true);
+    console.log(query);
+    const res = await axios.get(
+      `https://ninepointer-staging.herokuapp.com/api/v1/engineering/video/${query}`
+    );
+    setData(res.data.data);
+    console.log(res.data.data);
+    setLoading(false);
+  };
 
   return (
     <>
-      <h1>Videos</h1>
-      {loading && <h2>Loading...</h2>}
-      <div>
-        {data?.map((item, index) => {
-          return (
-            <>
-              <VideoCard
-                img_src={item.thumbnail}
-                cardTitle={item.title}
-                cardDescription={item.description}
-                cardAuthor='apple'
-                link={item.videoLink}
-              />
-            </>
-          );
-        })}
+      <div className={styles.container}>
+        <div className={styles.filters}>
+          <h1>Filters</h1>
+          <div className={styles.filterBox}>
+            {branch && <h2>Branch: {branch}</h2>}
+            <input
+              type='checkbox'
+              name='Mechanical'
+              id='Mechanical'
+              className='branch'
+              onChange={branchChange}
+            />
+            <label for='Mechanical'>Mechanical</label>
+            <br />
+            <input
+              type='checkbox'
+              name='Computer Science'
+              id='Computer Science'
+              className='branch'
+              onChange={branchChange}
+            />
+            <label for='Computer Science'>Computer Science</label>
+            <br />
+            <input
+              type='checkbox'
+              name='Electrical'
+              id='Electrical'
+              className='branch'
+              onChange={branchChange}
+            />
+            <label for='Electrical'>Electrical</label>
+            <br />
+          </div>
+          <div className={styles.filterBox}>
+            {type && <h2>Type: {type}</h2>}
+            <input
+              type='checkbox'
+              name='Topic Video'
+              id='Topic Video'
+              className='type'
+              onChange={branchChange}
+            />
+            <label for='Topic Video'>Topic Video</label>
+            <br />
+            <input
+              type='checkbox'
+              name='Engineering Life'
+              id='Engineering Life'
+              className='type'
+              onChange={branchChange}
+            />
+            <label for='Engineering Life'>Engineering Life</label>
+            <br />
+            <input
+              type='checkbox'
+              name='Solution Video'
+              id='Solution Video'
+              className='type'
+              onChange={branchChange}
+            />
+            <label for='Solution Video'>Solution Video</label>
+            <input
+              type='checkbox'
+              name='Others'
+              id='Others'
+              className='type'
+              onChange={branchChange}
+            />
+            <label for='Others'>Others</label>
+            <br />
+          </div>
+        </div>
+        <div className={styles.videos}>
+          <h1>Videos</h1>
+          {loading && <h2>Loading...</h2>}
+          <div>
+            {data?.map((item, index) => {
+              return (
+                <VideoCard
+                  key={index}
+                  img_src={item.thumbnail}
+                  cardTitle={item.title}
+                  cardDescription={item.description}
+                  cardAuthor='apple'
+                  link={item.videoLink}
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
 
       {/* <VideoCard
